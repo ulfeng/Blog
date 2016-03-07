31c1ae3fcc7fa320a77cb57f693f57760aad4289
Sun Lifeng <1104506375@qq.com>
2016-03-07 15:40:04 +0800

    JavaScript 类型转换
    String Number Boolean

--- test/2016-03-07.js
+++ test/2016-03-07.js
@@ -25,5 +25,37 @@ new Bar() instanceof Foo; // false
 new String('foo') instanceof String; // true
 new String('foo') instanceof Object; // true
 
-'foo' instanceof String; // false
-'foo' instanceof Object; // false
+'foo'
+instanceof String; // false
+'foo'
+instanceof Object; // false
+
+new Number(10) == 10; // Number.toString() 返回的字符串被再次转换为数字
+
+10 == '10'; // 字符串被转换为数字
+10 == '+10'; // 同上
+10 == '010'; // 同上
+isNaN(null) == false; // null 被转换为数字0
+
+10 == 010;
+10 == '-10';
+
+new Number(10) === 10; // False, 对象与数字的比较
+Number(10) === 10; // True, 数字与数字的比较
+new Number(10) + 0 === 10; // True,由于隐式的类型转化
+
++ '010' === 10;
+Number('010') === 10;
+parseInt('010', 10) === 10 // 用来转换为整数
+
+    + '010.2' === 10.2;
+Number('010.2') === 10.2;
+parseInt('010.2', 10) === 10;
+
+!!'foo'; // true
+!!'';    // false
+!!'0';   // true
+!!'1';   // true
+!!'-1';  // true
+!!{};    // true
+!!true;  // true
